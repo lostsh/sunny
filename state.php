1699b1df5e017b3eca3aa28934670e4ebae948fe
lost-guy <lost-guy@localhost.localdomain>
2019-05-15 12:01:55 +0200

    debut page php

---
+++ state.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset='utf-8'>
+	<title>Tracker Solaire</title>
+	<link rel="icon" href="assets/images/logo.ico"/>
+	<link rel="stylesheet" type="text/css" href="assets/css/barre.css"/>
+</head>
+<body>
+
+
+
+</body>
+<footer><!---====FOOTER====-->
+	<div>
+			<div class="text">
+		    TS4 - ISN © 2018-2019
+	    </div>
+	    <div class= "text">
+	    	|
+	    </div>
+			<div style="color:blue;text-decoration:underline;" class="text">
+	    	Léa JEAN
+	    </div>
+	    <div class= "text">
+	    	|
+	    </div>
+	    <div class= "text">
+	    	<a href="https://lanryu.000webhostapp.com/">Adélaïde LOUIS</a>
+	    </div>
+	    <div class= "text">
+	    	|
+	    </div>
+	    <div class= "text">
+	    	<a href="https://lost-sh.000webhostapp.com/">Yohann VERNHES</a>
+	    </div>
+	    <div class= "text">
+	    	|
+	    </div>
+	    <div class= "text">
+	    	 <a style="color: black;text-decoration: none;" href="https://lyceeduruy.fr/">Lycée Victor Duruy</a>
+	    </div>
+	    <div class= "accueil">
+	    	<a style="color: black;text-decoration: none;" href="index.html">ACCUEIL</a>
+			</div>
+	</div>
+</footer>
+<script type="text/javascript" src="assets/js/anim-fondHead.js"></script>
+</html>
